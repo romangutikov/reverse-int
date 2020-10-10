00a36123187938ab0913402d94591ab7d085e2b0
romangutikov <romangutikov@gmail.com>
2020-10-10 13:58:34 +0800

    fix: solution
    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
-}
+    let str = n.toString()
+    let res = '';
+    for (let i = str.length-1; i >= 0; i--) {
+        if (Number.isInteger(Number(str[i]))) res += str[i]
+    }
+    return +res
+  }
